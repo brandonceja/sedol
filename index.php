adba9cd95be21ec647b42d23e7267dbdc0ad6d01
Brandon Ceja <darth1701@hotmail.com>
2018-04-11 23:17:35 -0600

    hello world

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/fontawesome.min.css">
+	<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+	<title>Sedol</title>
+</head>
+<body>
+	<center>
+		<div id="wrap">
+			<div>
+				<h1 id="main-title">Título del Curso</h1>
+			</div>
+			<div id="navbar">
+				<div><a href="#">Contenido</a></div>
+				<div><a href="#">Índice</a></div>
+				<div><a href="#">Presentación</a></div>
+				<div><a href="#">Recursos-SAGE</a></div>
+				<div id="arrows">
+					<div><a href="#"><i class="fas fa-arrow-left"></i></a></div>
+					<div><a href="#"><i class="fas fa-arrow-up"></i></a></div>
+					<div><a href="#"><i class="fas fa-arrow-right"></i></a></div>
+				</div>
+			</div>
+			<div id="site">
+				<div id="left-column">
+					<div>
+						<div class="title">
+							<h3>1. Teoría básica</h3>
+						</div>
+						<div class="subtitle">
+						
+						</div>
+					</div>
+					<div>
+						<div class="title">
+							<h3>2. Método 2x2</h3>
+						</div>
+						<div class="subtitle">
+							
+						</div>
+					</div>
+					<div>
+						<div class="title">
+							<h3>3. Método nxn</h3>
+						</div>
+						<div class="subtitle">
+							
+						</div>
+					</div>
+				</div>
+				<div id="content">
+					<div><a href="#"><h2>1</h2> &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; TEORÍA BÁSICA</a></div>
+					<div><a href="#"><h2>2</h2> &nbsp; &nbsp; &nbsp; MÉTODO DEL EIGENVALOR PARA SISTEMAS 2x2</a></div>
+					<div><a href="#"><h2>3</h2> &nbsp; &nbsp; &nbsp; MÉTODO DEL EIGENVALOR PARA SISTEMAS NxN</a></div>
+				</div>
+			</div>
+		</div>
+	</center>
+</body>
+</html>
